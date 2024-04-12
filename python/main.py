ab1933b4dde1883759759a676a69a62e3e78d9d2
Caio Lüders <caioluders@Caios-MacBook-Air.local>
2024-04-12 02:09:27 -0300

    dockerize python, remove some

--- python/main.py
+++ python/main.py
@@ -31,9 +31,6 @@ import json
 
 # sanitizers
 import bleach
-import htmllaundry # .sanitize
-from html_sanitizer import Sanitizer
-from lxml.html.clean import clean_html
 
 # setup a mock django app
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoapp.settings")
@@ -46,139 +43,57 @@ from sanitizer.templatetags.sanitizer import (sanitize as django_sanitize, sanit
     escape_html, strip_filter, strip_html)
 
 from django.conf import settings
+from flask import Flask, request, jsonify
 
-def main():
+
+
+app = Flask(__name__)
+
+@app.route('/sanitize', methods=['POST'])
+def sanitize():
+	markups = request.json.get('markups')
 	
-	BASE_DIR= os.path.dirname(os.path.realpath(__file__))
-	INPUT_FILE_DEFAULT = os.path.join(os.path.dirname(BASE_DIR), "markups.txt")
-	OUTPUT_FILE_DEFAULT = os.path.join(BASE_DIR, "outputs/results.json")
-
-	p = argparse.ArgumentParser(description='This script tests the python sanitizers with the given markups.')
-	p.add_argument('--input', "-I",
-					metavar="FILE",
-					default=INPUT_FILE_DEFAULT,
-					help='path to input file. (default: %(default)s)',
-					type=str)
-
-	p.add_argument('--output', "-O",
-					metavar="FILE",
-					default=OUTPUT_FILE_DEFAULT,
-					help='path to output file. (default: %(default)s)',
-					type=str)
-
-
-
-	args= vars(p.parse_args())
-	
-	INPUT_FILE = args["input"]
-	OUTPUT_FILE = args["output"]
-
-
-	fd = open(INPUT_FILE, 'r')
-	lines = fd.readlines();
-	fd.close()
-
-
 	results = {
 		'bleach': [],
-		'lxml': [],
-		'htmlsanitizer': [],
-		'htmllaundry': [],
 		'django-html-sanitizer': [],
 		'django-html-escape': []
 	}
 
-	# # instantiate 
-	htmlSanitizor = Sanitizer()
-
-	for line in lines:
-		payload = line.strip().rstrip('\n').strip()
-
-
-		output = bleach.clean(payload)
+	
+	for markup in markups:
+		payload = markup.strip().rstrip('\n').strip()
 		try:
 			results['bleach'].append({
 				'input': payload,
-				'output': output
+				'output': bleach.clean(payload)
 			})
 		except:
 			results['bleach'].append({
 				'input': payload,
 				'output': 'error'
 			})
-
 		try:
-			output = htmlSanitizor.sanitize(payload)
-			results['htmlsanitizer'].append({
-				'input': payload,
-				'output': output
-			})
-		except:
-			results['htmlsanitizer'].append({
-				'input': payload,
-				'output': 'error'
-			})
-
-		try:
-			output = htmllaundry.sanitize(payload)
-			results['htmllaundry'].append({
-				'input': payload,
-				'output': output
-			})
-		except:
-			results['htmllaundry'].append({
-				'input': payload,
-				'output': 'error'
-			})	
-
-		try:
-			output = clean_html(payload)
-			results['lxml'].append({
-				'input': payload,
-				'output': output
-			})
-		except:
-			results['lxml'].append({
-				'input': payload,
-				'output': 'error'
-			})
-
-
-		try:
-			output = django_sanitize(payload)
 			results['django-html-sanitizer'].append({
 				'input': payload,
-				'output': output
+				'output': django_sanitize(payload)
 			})
 		except:
 			results['django-html-sanitizer'].append({
 				'input': payload,
 				'output': 'error'
-			})	
-
+			})
 		try:
-			output = escape_html(payload, allowed_tags=settings.SANITIZER_ALLOWED_TAGS, allowed_attributes=settings.SANITIZER_ALLOWED_ATTRIBUTES, allowed_styles=settings.SANITIZER_ALLOWED_STYLES)
 			results['django-html-escape'].append({
 				'input': payload,
-				'output': output
+				'output': escape_html(payload, allowed_tags=settings.SANITIZER_ALLOWED_TAGS, allowed_attributes=settings.SANITIZER_ALLOWED_ATTRIBUTES, allowed_styles=settings.SANITIZER_ALLOWED_STYLES)
 			})
 		except:
 			results['django-html-escape'].append({
 				'input': payload,
 				'output': 'error'
-			})	
+			})
+	
+	return jsonify(results)
 
-
-	with open(OUTPUT_FILE, 'w+', encoding='utf-8') as fd:
-		json.dump(results, fd, ensure_ascii=False, indent=4)
-
-
-
-if __name__ == "__main__":
-	main()
-
-
-
-
-
-
+if __name__ == '__main__':
+	app.run(host="0.0.0.0",port=3001)
